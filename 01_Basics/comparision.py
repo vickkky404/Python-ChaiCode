70ff1b63acdcc2f5761808c65b0a5ab465ffa9a2
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-22 22:34:14 +0530

    Add files via upload

---
+++ 01_Basics/comparision.py
@@ -0,0 +1,21 @@
+a = 1 < 3
+print(a)
+
+# false = 0
+# true = 1
+
+# comparision
+b = 2.0 == 2.0
+print(b)
+
+c = 4.0 != 5.0
+print(c)
+
+# chained comparision
+x = 2 
+y = 3 
+z = 4
+s =  x < y < z
+print(s)
+
+x < y and y < z
