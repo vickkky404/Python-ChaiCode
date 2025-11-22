8fcdb7629bf5d4097acdd07b55ddc5fa691e286e
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-22 23:01:39 +0530

    Add Boolean.py to demonstrate boolean operations

---
+++ 01_Basics/Boolean.py
@@ -0,0 +1,14 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+True == 1
+True
+False == 0
+True
+True == 0
+False
+True is 1
+False
+True + 4
+5
+False + 0
+0
