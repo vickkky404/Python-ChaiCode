800cc367b091c2899c6e93500c83c966de4dce45
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-22 22:56:17 +0530

    Add basic set operations examples in SetsBasics.py

---
+++ 01_Basics/SetsBasics.py
@@ -0,0 +1,17 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+setone = {1,2,3,4}
+setone = & {1 , 3}
+SyntaxError: invalid syntax
+setone  & {1 , 3}
+{1, 3}
+setone | {1,2}
+{1, 2, 3, 4}
+setone   {1 , 3 , 7}
+SyntaxError: invalid syntax
+setone |  {1 , 3 , 7}
+{1, 2, 3, 4, 7}
+setone - {1,2,3,4}
+set()
+type({})
+<class 'dict'>
