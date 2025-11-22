1ee29da0eab0375d213c8c90d53c746e7305599a
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-22 22:52:20 +0530

    Add BasicOperations.py with math and random examples

---
+++ 01_Basics/BasicOperations.py
@@ -0,0 +1,55 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2 + 1j
+(2+1j)
+import math
+import math.random
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    import math.random
+ModuleNotFoundError: No module named 'math.random'; 'math' is not a package
+oct(64)
+'0o100'
+import random
+random.random()
+0.3242545409862063
+random.random()
+0.333925309958906
+random.randint(1,3)
+1
+
+random.randint(1,3)
+1
+random.randint(1,3)
+3
+random.randint(1,3)
+2
+random.randint(1,3)
+1
+random.randint(1,3)
+1
+l1
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    l1
+NameError: name 'l1' is not defined
+
+
+l1 = ['lemon' , 'masala' , 'ginger' , 'mint']
+random.choice(l1)
+'lemon'
+random.choice(l1)
+'ginger'
+
+
+0.1 + 0.1 + 0.4
+0.6000000000000001
+0.1 + 0.1 + 0.1 - 0.3
+5.551115123125783e-17
+from decimal import Decimal
+Decimal('0.1') + Decimal('0.1')
+Decimal('0.2')
+from fractions import Fraction
+myFra = Fraction(2 , 7)
+myFra
+Fraction(2, 7)
