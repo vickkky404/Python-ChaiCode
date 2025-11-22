cd028177dbc1cc5d478061690aef0a36983e8766
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-22 21:57:23 +0530

    Add files via upload

---
+++ 01_Basics/numbers.py
@@ -0,0 +1,26 @@
+x = 2.23
+y = 3
+z = 4
+
+a = x + y
+print(a)
+
+
+# paranthesis are solved first
+
+
+# not a good practice
+# not used in production code..
+b =  (x + y) * z
+print(b)
+
+# good practices..
+int(2.23)
+str("Bastia Codes")
+float(40)
+
+q = 'chai' + 'codes'
+print(q)
+
+
+
