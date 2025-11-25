266ef14c11176d7fe5b288c37eabd06a4bdf86ff
Nalinikanta Bastia <nalinikantbastia@gmail.com>
2025-11-25 19:52:25 +0530

    Condational solution 3

---
+++ 02_condational/03_solution.py
@@ -0,0 +1,19 @@
+score = 90
+
+if score >= 101:
+    print("Print verify your grade again..")
+    exit()
+
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade: " , grade)
