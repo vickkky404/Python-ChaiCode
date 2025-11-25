1157af8dda43f761cc8b14a0af8f0a78ee047eb1
Nalinikanta Bastia <nalinikantbastia@gmail.com>
2025-11-25 20:00:24 +0530

    Condational solution 4

---
+++ 02_condational/04_solution.py
@@ -0,0 +1,14 @@
+# problem 3
+
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("Over Ripe")
+# else:
+#     print("No Information")
