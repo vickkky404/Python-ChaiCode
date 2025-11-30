cd49edb672ea5e399becfaa2572269ebdcd492cc
Nalinikanta Bastia <nalinikantbastia@gmail.com>
2025-11-30 10:50:00 +0530

    Prime Checker

---
+++ Practice/Prime_check.py
@@ -0,0 +1,10 @@
+n = 11
+if n <= 1:
+    print(n)
+else:
+    is_prime = True
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            is_prime = False
+            break
+    print(is_prime)
