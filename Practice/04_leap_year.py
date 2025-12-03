d44ea0d6aed204f5cb882093ad4fecf942fa62df
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-03 09:37:12 +0530

    Add leap year checker practice program

---
+++ Practice/04_leap_year.py
@@ -0,0 +1,9 @@
+# Leap Year Checker
+# A year is leap if divisible by 4, except centuries unless divisible by 400
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
