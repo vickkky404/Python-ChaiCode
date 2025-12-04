1c059d6520180262b5b65d7ab4a9946a4ed82c14
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-04 11:04:04 +0530

    Add files via upload

---
+++ 06_scopes/01_scopes.py
@@ -0,0 +1,27 @@
+username = "chaiaurcode"
+
+def  func():
+    username = "chai"
+    print(username)
+
+print(username)
+
+
+# calls the local function..
+func()
+
+x = 99
+
+# def func2(y):
+#     z = x+y
+#     return z
+
+# result = func2(1)
+# print (result)
+
+def func3():
+    x = 88
+
+print(88 )
+
+
