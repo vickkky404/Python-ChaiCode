ffdb8aa6fe5d372b08c210986f2f6f0d1afbf3ca
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-09 16:01:59 +0530

    Create ArrayOperation_Using_NumPy.py

---
+++ Data Structures and Practices/ArrayOperation_Using_NumPy.py
@@ -0,0 +1,13 @@
+# using numpy
+import numpy as np
+
+a = np.array([1,2,3,4,5])
+b = np.array([10,20,30,40])
+
+# element wise operations
+print(a + b)
+print(a * b)
+
+# slicing and reshaping
+print(a[1:3])
+print(a.reshape(2, 2))
