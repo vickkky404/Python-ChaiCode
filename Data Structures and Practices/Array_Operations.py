8acc11d31e86b4e3469809e5ae91079e98d9eabe
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-09 15:53:58 +0530

    Create Array_Operations.py

---
+++ Data Structures and Practices/Array_Operations.py
@@ -0,0 +1,31 @@
+# using python list..
+
+# creating the array using the list
+arr = [10,20,30,40,50]
+
+#accessing the element
+print(arr[0])
+print(arr[-2)
+print(arr[3])
+
+# updating the element..
+arr[1] = 11
+print(arr)
+
+#insert/appending
+arr.apppend(50)
+arr.insert(2 , 99)
+print(arr)
+
+# delete
+del arr[3]  #delete by index
+arr.remove(25) #delete by value
+x = arr.pop()  #remobes the last and returns it..
+
+# length
+print(len(arr))
+
+# slicing
+print(arr[1:4])  
+print(arr[::-1]) #reverse list
+ 
