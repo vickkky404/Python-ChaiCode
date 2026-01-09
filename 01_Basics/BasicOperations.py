e40cdc9ed3456d4c2092ed7239e6192cdd37a3b8
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-01-09 22:11:53 +0530

    Update BasicOperations.py

--- 01_Basics/BasicOperations.py
+++ 01_Basics/BasicOperations.py
@@ -1,3 +1,5 @@
+# python program
+
 Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 2 + 1j
