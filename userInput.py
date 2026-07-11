18025251833dd6320acedac2843be3e4c8c8d19e
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-07-11 07:42:43 +0530

    Add user input and sum calculation functionality

---
+++ userInput.py
@@ -0,0 +1,9 @@
+#store user inputs
+
+num1 = input('Enter first Number: ')
+num2 = input('Enter second Number: ')
+
+
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
